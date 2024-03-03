3ee79cc1f683c595b60ac07a60dc0782f692e7ae
Abhishek <abhishekbhosale.dev@gmail.com>
2024-03-03 18:08:33 +0530

    feat: dropdown menu complete
    added the content of the dropdown menu

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -1,7 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import { ReactNode, useState } from "react";
-import { FiChevronDown } from "react-icons/fi";
+import {
+  FiArrowRight,
+  FiBarChart2,
+  FiChevronDown,
+  FiHome,
+  FiPieChart,
+} from "react-icons/fi";
 import { AnimatePresence, easeIn, motion } from "framer-motion";
 
 export const DropDown = () => {
@@ -114,6 +121,25 @@ const Content = ({
       className="absolute left-0 top-[calc(100%_+_24px)] w-96 rounded-lg border border-neutral-600 bg-gradient-to-b from-neutral-900 via-neutral-900 to-neutral-800 p-4"
     >
       <Bridge />
+
+      {TABS.map((t) => {
+        return (
+          <div className="overflow-hidden" key={t.id}>
+            {selected === t.id && (
+              <motion.div
+                initial={{
+                  opacity: 0,
+                  x: dir === "l" ? 100 : dir === "r" ? -100 : 0,
+                }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.25, ease: "easeInOut" }}
+              >
+                <t.Component />
+              </motion.div>
+            )}
+          </div>
+        );
+      })}
     </motion.div>
   );
 };
@@ -122,21 +148,132 @@ const Bridge = () => {
   return <div className="absolute -top-[24px] left-0 right-0 h-[24px]" />;
 };
 
-const ExampleComponent = () => {
-  return <div>Hello world</div>;
+const Products = () => {
+  return (
+    <div>
+      <div className="flex gap-4">
+        <div>
+          <h3 className="mb-2 text-sm font-medium">Startup</h3>
+          <a href="#" className="mb-1 block text-sm text-neutral-400">
+            Bookkeeping
+          </a>
+          <a href="#" className="block text-sm text-neutral-400">
+            Invoicing
+          </a>
+        </div>
+        <div>
+          <h3 className="mb-2 text-sm font-medium">Scaleup</h3>
+          <a href="#" className="mb-1 block text-sm text-neutral-400">
+            Live Coaching
+          </a>
+          <a href="#" className="mb-1 block text-sm text-neutral-400">
+            Reviews
+          </a>
+          <a href="#" className="block text-sm text-neutral-400">
+            Tax/VAT
+          </a>
+        </div>
+        <div>
+          <h3 className="mb-2 text-sm font-medium">Enterprise</h3>
+          <a href="#" className="mb-1 block text-sm text-neutral-400">
+            White glove
+          </a>
+          <a href="#" className="mb-1 block text-sm text-neutral-400">
+            SOX Compliance
+          </a>
+          <a href="#" className="block text-sm text-neutral-400">
+            Staffing
+          </a>
+          <a href="#" className="block text-sm text-neutral-400">
+            More
+          </a>
+        </div>
+      </div>
+
+      <button className="ml-auto mt-4 flex items-center gap-1 text-sm text-indigo-300">
+        <span>View more</span>
+        <FiArrowRight />
+      </button>
+    </div>
+  );
+};
+
+const Pricing = () => {
+  return (
+    <div className="grid grid-cols-3 gap-4 divide-x divide-neutral-700">
+      <a
+        href="#"
+        className="flex w-full flex-col items-center justify-center py-2 text-neutral-400 transition-colors hover:text-neutral-50"
+      >
+        <FiHome className="mb-2 text-xl text-indigo-300" />
+        <span className="text-xs">Startup</span>
+      </a>
+      <a
+        href="#"
+        className="flex w-full flex-col items-center justify-center py-2 text-neutral-400 transition-colors hover:text-neutral-50"
+      >
+        <FiBarChart2 className="mb-2 text-xl text-indigo-300" />
+        <span className="text-xs">Scaleup</span>
+      </a>
+      <a
+        href="#"
+        className="flex w-full flex-col items-center justify-center py-2 text-neutral-400 transition-colors hover:text-neutral-50"
+      >
+        <FiPieChart className="mb-2 text-xl text-indigo-300" />
+        <span className="text-xs">Enterprise</span>
+      </a>
+    </div>
+  );
+};
+
+const Blog = () => {
+  return (
+    <div>
+      <div className="grid grid-cols-2 gap-2">
+        <a href="#">
+          <img
+            className="mb-2 h-14 w-full rounded object-cover"
+            src="/imgs/blog/4.png"
+            alt="Placeholder image"
+          />
+          <h4 className="mb-0.5 text-sm font-medium">Lorem ipsum dolor</h4>
+          <p className="text-xs text-neutral-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illo
+            quidem eos.
+          </p>
+        </a>
+        <a href="#">
+          <img
+            className="mb-2 h-14 w-full rounded object-cover"
+            src="/imgs/blog/5.png"
+            alt="Placeholder image"
+          />
+          <h4 className="mb-0.5 text-sm font-medium">Lorem ipsum dolor</h4>
+          <p className="text-xs text-neutral-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illo
+            quidem eos.
+          </p>
+        </a>
+      </div>
+      <button className="ml-auto mt-4 flex items-center gap-1 text-sm text-indigo-300">
+        <span>View more</span>
+        <FiArrowRight />
+      </button>
+    </div>
+  );
 };
 
 const TABS = [
   {
     title: "Products",
-    Component: ExampleComponent,
+    Component: Products,
   },
   {
     title: "Pricing",
-    Component: ExampleComponent,
+    Component: Pricing,
   },
   {
     title: "Blog",
-    Component: ExampleComponent,
+    Component: Blog,
   },
 ].map((n, idx) => ({ ...n, id: idx + 1 }));
